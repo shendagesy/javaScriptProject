66f9ff1be52ced1533f04e02e2d6ad7421705fe3
shendagesy <47170673+shendagesy@users.noreply.github.com>
2020-09-11 12:48:09 +0530

    Minor code changes and comment added.

--- javascript/script.js
+++ javascript/script.js
@@ -1,28 +1,20 @@
 let globalData;
 
+// This method is used for convert JSon DATA
 function csvJSON(csv) {
 
   var lines = csv
-
   var result = [];
-
   var headers = lines[0].split(",");
-
   for (var i = 1; i < lines.length; i++) {
-
     var obj = {};
     var currentline = lines[i].split(",");
-
     for (var j = 0; j < headers.length; j++) {
       obj[headers[j]] = currentline[j];
     }
-
     result.push(obj);
-
   }
   globalData = result;
-  //return result; //JavaScript object
-  // return JSON.stringify(result); //JSON
 }
 
 function Upload() {
